18c0e04da24cb5537631028a43072c32f7a56c46
BALAJI K <125179796+Balaji-K369@users.noreply.github.com>
2023-06-19 19:38:14 +0530

    Add files via upload

---
+++ app/conditions/conditions.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-conditions',
+  templateUrl: './conditions.component.html',
+  styleUrls: ['./conditions.component.css']
+})
+export class ConditionsComponent {
+
+}
